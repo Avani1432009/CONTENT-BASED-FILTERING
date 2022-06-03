5954229ebe291baa7b8e6e810e07f5d542ae01a9
Avani1432009 <85622838+Avani1432009@users.noreply.github.com>
2022-06-03 18:11:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 from flask import Flask, jsonify, request
 import csv
 
-all_articles = []
+all_movies = []
 
-with open("articles.csv") as f:
+with open("movies.csv") as f:
     reader = csv.reader(f)
     data = list(reader)
-    all_articles = data[1:]
+    all_movies = data[1:]
 
-liked_articles = []
-not_liked_articles = []
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
 
 app = Flask(__name__)
 
-@app.route("/get-article")
-def get_article():
+@app.route("/get_movie")
+def get_movie():
     return jsonify({
-        "data": all_articles[0],
-        "status": "success"
+        "data": all_movies[0],
+        "status":"success"
     })
 
-@app.route("/liked-article", methods=["POST"])
-def liked_article():
-    article = all_articles[0]
-    liked_articles.append(article)
-    all_articles.pop(0)
+@app.route("/liked_movie",methods = ["POST"])
+def liked_movies():
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    liked_movies.append(movie)
     return jsonify({
-        "status": "success"
-    }), 201
+        "status":"success"
+    }),201
 
-@app.route("/unliked-article", methods=["POST"])
-def unliked_article():
-    article = all_articles[0]
-    not_liked_articles.append(article)
-    all_articles.pop(0)
+@app.route("/unliked_movie",methods = ["POST"])
+def unliked_movies():
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    not_liked_movies.append(movie)
     return jsonify({
-        "status": "success"
-    }), 201
+        "status":"success"
+    }),201
+
+@app.route("/did-not-watch",methods = ["POST"])
+def did_not_watch():
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    did_not_watch.append(movie)
+    return jsonify({
+        "status":"success"
+    }),201
 
 if __name__ == "__main__":
-    app.run()
+   app.run()
